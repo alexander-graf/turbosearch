813d093fd86ef0ef9543c0695d8940aa093ce561
Alexander Graf <info@alexander-graf.de>
2023-10-04 01:27:43 +0200

    paar buttons und so

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use fltk::{
 };
 
 fn main() {
+    // Startvariablen für counter
+    let mut x = 0;
+    let mut y = 0;
+
     let app = App::default();
     let mut win = Window::default()
         .with_size(600, 400)
@@ -26,32 +30,76 @@ fn main() {
         .with_size(100, 30)
         .with_pos(10, 120)
         .with_label("Button 1");
+    let original_label1 = button1.label();
+    button1.set_callback(move |b| {
+        x += 1;
+        b.set_label(&x.to_string());
+    });
 
     let mut button2 = Button::default()
         .with_size(100, 30)
         .with_pos(10, 160)
         .with_label("Button 2");
+    let original_label2 = button2.label();
+    button2.set_callback(move |b| {
+        y += 1;
+        b.set_label(&y.to_string());
+    });
+use fltk::{
+    app::App,
+    button::Button,
+    input::{Input, InputType},
+    prelude::{WidgetExt, GroupExt},
+    window::Window,
+};
 
-    let mut input3 = Input::default()
+fn main() {
+    let app = App::default();
+    let mut win = Window::default()
+        .with_size(600, 400)
+        .with_label("FLTK GUI");
+
+    let mut input1 = Input::default()
         .with_size(100, 40)
-        .with_pos(200, 10)
-        .with_label("Input 3");
+        .with_pos(10, 10)
+        .with_label("Input 1");
 
-    let mut input4 = Input::default()
+    let mut input2 = Input::default()
         .with_size(100, 40)
-        .with_pos(200, 60)
-        .with_label("Input 4");
+        .with_pos(10, 60)
+        .with_label("Input 2");
 
-    let mut button3 = Button::default()
+    let mut button1 = Button::default()
         .with_size(100, 30)
-        .with_pos(200, 120)
-        .with_label("Button 3");
+        .with_pos(10, 120)
+        .with_label("Button 1");
+    let original_label1 = button1.label().to_string();
+    button1.set_callback(move |b| {
+        if b.label() == "Pressed" {
+            b.set_label(&original_label1);
+        } else {
+            b.set_label("Pressed");
+        }
+    });
 
-    let mut button4 = Button::default()
+    let mut button2 = Button::default()
         .with_size(100, 30)
-        .with_pos(200, 160)
-        .with_label("Button 4");
+        .with_pos(10, 160)
+        .with_label("Button 2");
+    let original_label2 = button2.label().to_string();
+    button2.set_callback(move |b| {
+        if b.label() == "Pressed" {
+            b.set_label(&original_label2);
+        } else {
+            b.set_label("Pressed");
+        }
+    });
 
+    win.end();
+    win.show();
+
+    app.run().unwrap();
+}
     win.end();
     win.show();
 
