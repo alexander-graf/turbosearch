0f2792accca4a2cd51d48462f4ab2ba4b93ad78b
Alexander Graf <info@alexander-graf.de>
2023-10-04 00:48:02 +0200

    gut soweit

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,26 @@ fn main() {
         .with_pos(10, 160)
         .with_label("Button 2");
 
+    let mut input3 = Input::default()
+        .with_size(100, 40)
+        .with_pos(200, 10)
+        .with_label("Input 3");
+
+    let mut input4 = Input::default()
+        .with_size(100, 40)
+        .with_pos(200, 60)
+        .with_label("Input 4");
+
+    let mut button3 = Button::default()
+        .with_size(100, 30)
+        .with_pos(200, 120)
+        .with_label("Button 3");
+
+    let mut button4 = Button::default()
+        .with_size(100, 30)
+        .with_pos(200, 160)
+        .with_label("Button 4");
+
     win.end();
     win.show();
 
